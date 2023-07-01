0e72ae0b428b41b1131b5d901d2ab8270ad0b80a
Sinixon <joeyburgt@hotmail.com>
2023-07-01 21:01:06 +0200

    Finish exercise ten

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS"
+	const fib = [0, 1];
+	for (let i = 1; i < number; i++) {
+		fib.push(fib[i] + fib[i -1]);
+	}
+	return fib[number];
 };
 
 // Do not edit below this line
